7aa38c592bfb4cb8fc79d7161734ddce86862f9d
nedarctic <nedarctic@gmail.com>
2024-10-18 19:18:54 +0300

    Added logic for checking for main file and the ngAfterViewInit function together with loading the iframe

--- index.js
+++ index.js
@@ -1,38 +1,55 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   const url = 'https://22bet.co.ke/othergames?product=849&game=68089';
 
+  let jsFileContent = ''; // Variable to store the JS file content
+
+  page.on('response', async (response) => {
+    const requestUrl = response.url();
+
+    // Check if the URL contains the specific JS file name
+    if (requestUrl.includes('main.c5fc690cdf2bb7af.js')) {
+      console.log('JS file found:', requestUrl);
+
+      try {
+        // Get the JS file content
+        jsFileContent = await response.text();
+        console.log('JavaScript file content captured.');
+
+        // Check if the file contains the method ngAfterViewInit
+        const hasNgAfterViewInit = jsFileContent.includes('ngAfterViewInit');
+        console.log('ngAfterViewInit found:', hasNgAfterViewInit);
+      } catch (error) {
+        console.error('Error retrieving JS file content:', error);
+      }
+    }
+  });
+
+  // Navigate to the URL
   await page.goto(url);
+  console.log('Navigated to the page.');
+
+  // Wait for the iframe to load
   await page.waitForSelector('iframe');
   const iframeSelector = await page.$('iframe');
   const iframe = await iframeSelector.contentFrame();
+  console.log('Iframe loaded.');
 
   // Title
   const iframeTitle = await iframe.title();
-  console.log('iframe title:', iframeTitle);
+  console.log('Title:', iframeTitle);
 
-  // URL
-  const iframeURL = await iframe.evaluate(() => window.location.href);
-  console.log('iframe URL:', iframeURL);
-
-  // Content
-  const iframeContent = await iframe.content();
-  console.log("iframe content:", iframeContent);
-
-  //images
-  await iframe.waitForSelector('img');
-  const iframeImages = await iframe.evaluate(() => Array.from(document.querySelectorAll('img')).map(img => ({ src: img.src, alt: img.alt })));
-  console.log('iframe images:', iframeImages);
-
-  // HTML
-  await iframe.waitForSelector('body');
-  const iframeHTML = await iframe.evaluate(() => document.body.innerHTML);
-  console.log('iframe HTML:', iframeHTML);
-
+  // Give some time for responses to be captured
   new Promise(resolve => setTimeout(resolve, 5000));
 
+  // If the JS file content was not found or no ngAfterViewInit
+  if (!jsFileContent) {
+    console.log('JS file was not intercepted or captured.');
+  }
+
+  // Close the browser
   await browser.close();
 })();
